fc7981ed897a5cd70551a9a650e33cce5dc27caa
nathias <nihilnegativum@protonmail.com>
2018-06-21 13:21:42 +0200

    recursive replying prevention

--- redditbot.py
+++ redditbot.py
@@ -21,12 +21,17 @@ def bot_login():
 def run_bot(reddit_instance):
     """bot implementation"""
 
+    comments_replied_to = []
+    keyword = 'bot'
+
     print("Obtaining 25 comments ...")
     for comment in reddit_instance.subreddit('test').comments(limit=25):
-        if "bot" in comment.body:
-            print("String found in " + comment.id)
-            comment.reply("wwaaat")
-            print("Replied to comment" + comment.id)
+        if comment.id not in comments_replied_to:
+            if keyword in comment.body:
+                print("String %s found in " % keyword + comment.id)
+                # comment.reply("wwaaat")
+                print("Replied to comment" + comment.id)
+                comments_replied_to.append(comment.id)
 
     print("Sleeping for 10 seconds ...")
     time.sleep(10)
